32efcbce5c461d6cb44600771620a65fdc44841e
Antonio Vázquez <antoniovazquezblanco@gmail.com>
2024-03-22 17:35:04 +0100

    Add a verbose flag

--- BlueSpy.py
+++ BlueSpy.py
@@ -52,6 +52,14 @@ def main():
         dest="sink",
         default="alsa_output.pci-0000_00_05.0.analog-stereo",
     )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="Show verbose output",
+        dest="verbose",
+        default=False,
+        action='store_true'
+    )
 
     args = parser.parse_args()
 
@@ -66,22 +74,22 @@ def main():
         f"[{bcolors.OK_GREEN}I{bcolors.ENDC}] Avoiding authentication with {args.address}..."
     )
     print(f"[{bcolors.OK_GREEN}I{bcolors.ENDC}] Generating shared key...")
-    pair(BluezTarget(args.address, args.address_type), verbose=False)
+    pair(BluezTarget(args.address, args.address_type), verbose=args.verbose)
     print(f"[{bcolors.WARNING}!{bcolors.ENDC}] Key generated")
     print(f"[{bcolors.OK_GREEN}I{bcolors.ENDC}] Establishing connection...")
     time.sleep(1)
-    connect(BluezTarget(args.address, args.address_type), verbose=False)
+    connect(BluezTarget(args.address, args.address_type), verbose=args.verbose)
     print(f"[{bcolors.OK_GREEN}I{bcolors.ENDC}] Starting audio recording...")
     print(f"[{bcolors.WARNING}!{bcolors.ENDC}] Recording!")
     time.sleep(1)
-    record(BluezTarget(args.address), outfile=args.outfile, verbose=False)
+    record(BluezTarget(args.address), outfile=args.outfile, verbose=args.verbose)
     print(f'[{bcolors.WARNING}!{bcolors.ENDC}] Recording stored in "{args.outfile}"')
 
     print(f"[{bcolors.OK_BLUE}?{bcolors.ENDC}] Play audio back? ")
     option = input("[Y/n] ") or "y"
     if option.lower() in ("y", "yes"):
         print(f"[{bcolors.WARNING}!{bcolors.ENDC}] Playing audio back!")
-        playback(args.sink, args.outfile, verbose=False)
+        playback(args.sink, args.outfile, verbose=args.verbose)
     print(f"[{bcolors.OK_GREEN}I{bcolors.ENDC}] Exiting")
 
 
